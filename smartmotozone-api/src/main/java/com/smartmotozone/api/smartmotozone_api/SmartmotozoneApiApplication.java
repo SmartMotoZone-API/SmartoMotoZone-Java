66a4dbec6c2af6f9e45fb3f42c54473c53be6a82
ArthurMitsuoYamamoto <arthuryamamoto0@gmail.com>
2025-05-20 14:56:06 -0300

    feat: created project smartmotozone using Spring and add dependencies

---
+++ smartmotozone-api/src/main/java/com/smartmotozone/api/smartmotozone_api/SmartmotozoneApiApplication.java
@@ -0,0 +1,13 @@
+package com.smartmotozone.api.smartmotozone_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartmotozoneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartmotozoneApiApplication.class, args);
+	}
+
+}
---
+++ smartmotozone-api/src/test/java/com/smartmotozone/api/smartmotozone_api/SmartmotozoneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartmotozone.api.smartmotozone_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartmotozoneApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
