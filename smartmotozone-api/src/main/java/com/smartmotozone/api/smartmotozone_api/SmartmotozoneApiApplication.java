530bfac4783eb8eef9332f1907c0707cf6218cae
ArthurMitsuoYamamoto <arthuryamamoto0@gmail.com>
2025-05-20 15:17:12 -0300

    feat: added caching in public class SmartmotozoneApiApplication and installed java (oracle-sdk24)

--- smartmotozone-api/src/main/java/com/smartmotozone/api/smartmotozone_api/SmartmotozoneApiApplication.java
+++ smartmotozone-api/src/main/java/com/smartmotozone/api/smartmotozone_api/SmartmotozoneApiApplication.java
@@ -2,8 +2,10 @@ package com.smartmotozone.api.smartmotozone_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SmartmotozoneApiApplication {
 
 	public static void main(String[] args) {
